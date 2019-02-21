1f90eff8643209f7480782eda9f3b5e8fd17cf7a
James Ritchie <james.g.ritchie@baesystems.com>
2019-02-21 13:36:08 +0000

    logs when root cert requested

--- server/server.go
+++ server/server.go
@@ -35,6 +35,7 @@ func certProvider(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		{
+			log.Println("root cert requested")
 			data = map[string]interface{}{
 				"Certificate": readFile("root.crt"),
 			}
